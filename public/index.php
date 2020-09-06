b23e040d710ca213ceb1e0c2665fa1a23ccf8e64
andrey <gusevandrey1988@gmail.com>
2020-09-06 16:42:50 +0300

    Вс сен  6 16:43:24 MSK 2020

--- public/index.php
+++ public/index.php
@@ -6,9 +6,54 @@
 	</head>
 	<body>
 		<header>
+			<div><a href="#">Вход</a></div>
+			<div><a href="#">Регистрация</a></div>
+			<nav></nav>
 		</header>
 
+		<!-- Список услуг -->
+		<section>
+			<h1>Выберите услугу:</h1>
+			<div>
+				<span>Услуга 1</span>
+				<input value="sev1" type="radio" name="typeOfService">
+			</div>
+			<div>
+				<span>Услуга 2</span>
+				<input value="sev2" type="radio" name="typeOfService">
+			</div>
+			<div>
+				<span>Услуга 2</span>
+				<input value="sev2" type="radio" name="typeOfService">
+			</div>
+		</section>
+
+		<!-- Список новостей -->
+		<section>
+			<h1>Список новостей:</h1>
+			<article>
+				<h1>В Анапе устраняют последствия мощного ливня</h1>
+				<p>Сегодня утром в течение получаса на Анапу обрушилось 39 мм осадков – больше месячной нормы.</p>
+			</article>
+			<article>
+				<h1>На Кубани за сутки подтверждено 93 случая коронавирусной инфекции</h1>
+				<p>Среди них 56 женщин, 37 мужчин в возрасте до 85 лет.</p>
+			</article>
+		</section>
+
+		<!-- Список ЧАВо -->
+		<section>
+			<h1>Часто задаваемые вопросы:</h1>
+			<ul>
+				<li>Как записатся на услугу?</li>
+				<li>Сайт не работает, что делать?</li>
+			</ul>
+		</section>
+
 		<footer>
+			<div>Сайт разработан компанией ООО "Лучшая компания в мире"&reg;</div>	
+			<div>Администратор сайта <a href="mailto:vasiapupkinne@gmail.com">vasiapupkinne@gmail.com</a><div>
+			<div><small>&copy;Copyright 2020</small></div>
 		</footer>
 	</body>
 </html>
