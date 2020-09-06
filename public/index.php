9c729d0c6e2a22030246fbf821562feae1852055
andrey <gusevandrey1988@gmail.com>
2020-09-06 16:12:49 +0300

    start project

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Сайт муниципальных услуг</title>
+	</head>
+	<body>
+		<header>
+		</header>
+
+		<footer>
+		</footer>
+	</body>
+</html>
